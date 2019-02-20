d5de9d604f6eb46c700e7467af1ed2e01f1a3d97
Donchi4all <donchi4all@gmail.com>
2019-02-20 01:51:15 +0100

    Add project files.

---
+++ PropertyClasses/Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyClasses
+{
+    public class Class1
+    {
+        //attributes/ fields
+        private string fname;
+        private string lname;
+        private decimal noofRoom;
+        public const decimal roomRent = 500;
+        private decimal totalCost;
+        private string condimentList;
+
+        //properties
+        public string Fname
+        {
+            get
+            {
+                return fname;
+            }
+            set
+            {
+                fname = value;
+            }
+        }
+
+
+        public string Lname
+        {
+            get
+            {
+                return lname;
+            }
+            set
+            {
+                lname = value;
+            }
+        }
+
+
+        public decimal NoofRoom
+        {
+            get
+            {
+                return noofRoom;
+            }
+            set
+            {
+                noofRoom = value;
+            }
+        }
+
+
+        //buy method
+        public decimal Buy()
+        {
+
+            return totalCost = this.noofRoom * roomRent;
+        }
+
+        //clear button
+        public string Clear()
+        {
+            return null ;
+        }
+
+        //clear num
+        public decimal ClearNum()
+        {
+            return 0;
+        }
+       
+
+        public string CondimentsList
+        {
+            get
+            {
+                condimentList = string.Empty;//refreshes to empty list
+                if (Fname != null)
+                    condimentList += fname + "  ";
+                if (Lname != null)
+                    condimentList += lname + " ";
+
+                return condimentList;
+            }
+            set
+            {
+                condimentList = value;
+            }
+        }
+
+
+
+        //constructor
+        public Class1()
+        {
+            Fname = Lname = null; NoofRoom = 0;
+        }
+
+        //overloaded constructor
+        public Class1(string fn, string ln, decimal nor)
+        {
+            Fname = fn;
+            Lname = ln;
+            NoofRoom = nor;
+
+        }
+
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+            output = "Thank You" + "  ";
+            if (CondimentsList != string.Empty)
+                output += " " + CondimentsList;
+            return output;
+        }
+
+
+
+    }
+}
---
+++ RentRoom/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using PropertyClasses;
+
+namespace RentRoom
+{
+    public partial class Form1 : Form
+    {
+        private const decimal ROOMRENT = 500;
+        Class1 class1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+
+            Buy();
+
+
+
+            //decimal total = nudNoRoom.Value * ROOMRENT;
+            //txtRoomRent.Text = nudNoRoom.Value.ToString();
+            //txtTotal.Text = total.ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+            //lblThankYou.Text = "Thank You" + "  " + txtFname.Text + "  " + txtLname.Text;
+            //btnClear.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clear();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("All fields are empty");
+            }
+
+            //txtFname.Clear();
+            //txtLname.Clear();
+            //txtTotal.Clear();
+            //txtRoomRent.Clear();
+            //lblThankYou.Text = "";
+            //nudNoRoom.Value = 0;
+            //txtFname.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+
+            if (keyData == Keys.Enter)
+            {
+
+                Buy();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+
+
+        public void Buy()
+        {
+            class1 = new Class1(txtFname.Text.ToString(),
+                                                    txtLname.Text.ToString(),
+                                                    nudNoRoom.Value
+                                                     );
+
+            txtRoomRent.Text = class1.NoofRoom.ToString();
+            txtTotal.Text = class1.Buy().ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+            lblThankYou.Text = class1.ToString();
+            btnClear.Focus();
+        }
+
+
+        private void Clear()
+        {
+            class1 = new Class1();
+            txtFname.Text = class1.Clear();
+            txtLname.Text = class1.Clear();
+            txtTotal.Text = class1.ClearNum().ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+            txtRoomRent.Text = class1.ClearNum().ToString();
+            lblThankYou.Text = class1.Clear();
+            nudNoRoom.Value = class1.ClearNum();
+            txtFname.Focus();
+        }
+    }
+}
